5fd04376d2334507b4e44402ac91b7e9e42b9e0f
Paulo Sergio <pscosta2008@gmail.com>
2024-05-31 00:37:40 -0300

    feat: adding Spring boot

---
+++ hastype/src/main/java/com/hastype/hastype/HastypeApplication.java
@@ -0,0 +1,13 @@
+package com.hastype.hastype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HastypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HastypeApplication.class, args);
+	}
+
+}
---
+++ hastype/src/test/java/com/hastype/hastype/HastypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastype.hastype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HastypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
